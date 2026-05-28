7ebce89c97961398b58af30b02b9f3f2ac303529
Michel Detiilli <detiillimichel@gmail.com>
2026-05-28 14:42:37 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'calculo-game-v1';
+const ASSETS = [
+  'index.html',
+  'about.html',
+  'style.css',
+  'script.js',
+  'manifest.json',
+  'icon.png'
+];
+
+// Instalação do Service Worker e Armazenamento em Cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Ativação e Limpeza de Caches Antigos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptação de Requisições para Funcionar Offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
